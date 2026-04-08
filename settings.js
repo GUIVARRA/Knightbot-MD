62cba4edb9665ff4bb1d759328f5765fa37c9d00
GUIVARRA <cherywidson331@gmail.com>
2026-04-08 10:59:23 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'SAÏVARRA MD',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "SAÏVARRA MD",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '8297010740', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
